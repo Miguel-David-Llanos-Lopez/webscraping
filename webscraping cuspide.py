b9db4a3cbcca7ae4b98b7d8d07372ac23cb9b75e
Miguel-David-Llanos-Lopez <llanoslopezmigueldavid@gmail.com>
2024-09-18 21:20:54 -0500

    funciones extra y coneccion con MySQL añadidos

--- webscraping cuspide.py
+++ webscraping cuspide.py
@@ -37,5 +37,17 @@ datos_errores = {'titulo':error_titulo, 'url':error_url}
 df_errores = pd.DataFrame(datos_errores)
 df = pd.DataFrame(datos)
 
+url2 = "https://www.xe.com/es/currencyconverter/convert/?Amount=1&From=USD&To=ARS"
+response2 = requests.get(url2)
+dolares = bs(response2.content, "html.parser")
+dolar_a_arg = dolares.find(class_="sc-295edd9f-1 jqMUXt").get_text().replace(",",".").split(" ")
+dolar_a_arg = float(dolar_a_arg[0])
+
+df["precio en dolares"] = round(df["precio"] / dolar_a_arg, 2)
+print(dolar_a_arg)
 print(df)
 print(df_errores)
+
+conexcion = 'mysql+mysqlconnector://root:root1234@localhost/webscrapping_cuspide'
+df.to_sql(name='webscraping', con=conexcion, if_exists="replace", index=False)
+df_errores.to_sql(name='errores_webscraping', con=conexcion, if_exists="replace", index=False)
